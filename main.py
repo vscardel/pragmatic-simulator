6549df90d57345626368058566a9674c7879df34
João Victor <jv.joaocoelho@gmail.com>
2026-01-15 10:35:09 -0300

    add probabilities to sensors

--- main.py
+++ main.py
@@ -1,19 +1,22 @@
 import uuid
 import random
-from simulator import SimulationEngine
+from production_plant import ProductionPlant
 from sensors import Sensor, SensorRoleEnum, SensorTypeEnum
 from broker import Broker
 import time
+from globals import Globals
+from actuator import Actuator
 
 NUMBER_OF_SENSORS = 5
+TIME_STEPS = 1000 # We can model it to be milliseconds
 
 
-
-def initialize_sensors(simulator: SimulationEngine, broker: Broker):
+def initialize_sensors(plant: ProductionPlant, broker: Broker):
     for _ in range(NUMBER_OF_SENSORS):
+        
         sensor = Sensor(
             sensor_id=uuid.uuid4(),
-            sensor_type=random.choice(list(SensorTypeEnum)),
+            sensor_type=SensorTypeEnum.TEMPERATURE,
             role=random.choice(list(SensorRoleEnum)),
             operating_range={ # Valores mockados e estaticos, mudar depois
                 "normal": (50, 60),
@@ -21,27 +24,34 @@ def initialize_sensors(simulator: SimulationEngine, broker: Broker):
                 "critical": (30, 95)
             },
             mean_value=55,
+            sampling_interval=random.randint(1, 5000) # Sensors that send data in a interval between 1 ms and 5 seconds
         )   
-        simulator.add_sensor(sensor)
+        plant.add_sensor(sensor)
         broker.subscribe(sensor)
                 
 
 if __name__ == "__main__":
-    simulator = SimulationEngine()
+    plant = ProductionPlant()
     broker = Broker()
-    initialize_sensors(simulator, broker)
+    actuator = Actuator(plant)
+    initialize_sensors(plant, broker)
 
-    while True: # Passos de tempo
+    for lt in range(TIME_STEPS): # Passos de tempo
+        Globals.time += 1
 
-        for id, sensor in simulator.sensors.items():
+        for id, sensor in plant.sensors.items():
+            sensor.update_state_by_probabilities()
+            sensor.adjust_probabilities_by_time_passing() # It will degradate over time (by the use)
+            
+            current_sensors_message = sensor.send_data()
+            if current_sensors_message is None:
+                continue
+            broker.publish(sensor.sensor_id, current_sensors_message)
 
-            current_sensors_messages = sensor.send_data()
-            # publish each generated message to broker
-            for message in current_sensors_messages:
-                broker.publish(sensor.sensor_id, sensor.role, message)
-
+    
         messages = broker.flush()
         # this will need to modulate the transition probabilities for the messages to matter
         # to the environment
-        simulator.step(messages)
+        actuator.step(messages)
+        print(f"Global State: {actuator.global_state}")
         time.sleep(0.5)
--- sensors.py
+++ sensors.py
@@ -3,12 +3,14 @@ import datetime
 import uuid
 from enum import Enum
 from typing import Literal
+from globals import Globals
+import time
 
 class SensorStateEnum(Enum):
-    NORMAL = 'NORMAL'
-    DEGRADED = 'DEGRADED'
-    CRITICAL = 'CRITICAL'
-    FAILURE = 'FAILURE'
+    NORMAL = 0
+    DEGRADED = 1
+    CRITICAL = 2
+    FAILURE = 3
 
 class SensorRoleEnum(Enum):
     CRITICAL = 0
@@ -30,7 +32,7 @@ class Sensor:
                  role: SensorRoleEnum,  # A relevância do sensor
                  operating_range: dict[Literal["normal", "degraded", "critical"], tuple[int, int]],
                  mean_value: float,
-                 
+                 sampling_interval: int = 0, # 0 = every time step
                 ):
         self.sensor_id = sensor_id
         self.sensor_type = sensor_type
@@ -39,27 +41,58 @@ class Sensor:
         self.mean_value = mean_value
         self.standard_deviation = (operating_range['normal'][1] - operating_range['normal'][0]) / 2
         self.local_state = SensorStateEnum.NORMAL
+        self.sampling_interval = sampling_interval
         self.__initialize_transition_probabilities()
 
 
     def __initialize_transition_probabilities(self):
-        self.transition_probabilities = {
+        self.original_transition_probabilities = { # Values when the machine is normal
             SensorStateEnum.NORMAL: {
-                SensorStateEnum.DEGRADED: 0.1
+                SensorStateEnum.DEGRADED: random.uniform(0.008, .012) # 0.8% to 1.2%
             },
             SensorStateEnum.DEGRADED: {
-                SensorStateEnum.CRITICAL: 0.2,
-                SensorStateEnum.NORMAL: 0.4
+                SensorStateEnum.CRITICAL: random.uniform(0.08, 0.16), # 8% to 16%
+                SensorStateEnum.NORMAL: random.uniform(0.001, 0.002) # 0.1% to 0.2%
             },
             SensorStateEnum.CRITICAL: {
-                SensorStateEnum.FAILURE: 0.3,
-                SensorStateEnum.DEGRADED: 0.5
-            },
-            SensorStateEnum.FAILURE: {
-                SensorStateEnum.CRITICAL: 0.6
+                SensorStateEnum.FAILURE: random.uniform(0.15, 0.4), # 15% to 40%
+                SensorStateEnum.DEGRADED: random.uniform(0.001, 0.002) # 1% to 2%
             }
         }
+        self.transition_probabilities = self.original_transition_probabilities
 
+        
+    def adjust_probabilities_by_time_passing(self):
+        if (self.local_state == SensorStateEnum.NORMAL):
+            self.transition_probabilities[SensorStateEnum.NORMAL][SensorStateEnum.DEGRADED] += 0.0001 # increase by 0.01%
+        
+        if (self.local_state == SensorStateEnum.DEGRADED):
+            self.transition_probabilities[SensorStateEnum.DEGRADED][SensorStateEnum.CRITICAL] += 0.0002 # increase by 0.02%
+            self.transition_probabilities[SensorStateEnum.DEGRADED][SensorStateEnum.NORMAL] -= 0.0001 # decrease by 0.01%
+        
+        if (self.local_state == SensorStateEnum.CRITICAL):
+            self.transition_probabilities[SensorStateEnum.CRITICAL][SensorStateEnum.FAILURE] += 0.0004 # increase by 0.04%
+            self.transition_probabilities[SensorStateEnum.CRITICAL][SensorStateEnum.DEGRADED] -= 0.0002 # decrease by 0.02%
+        
+    def update_state_by_probabilities(self):
+        rand_value = random.uniform(0, 1)
+        
+        possibly_states = self.transition_probabilities[self.local_state] if self.local_state != SensorStateEnum.FAILURE else {}
+        
+        
+        for state, probability in possibly_states.items():
+            if rand_value < probability:
+                self.local_state = state
+                print(f"Sensor {self.sensor_id} updated state to {self.local_state}")
+                print(self.transition_probabilities)
+                time.sleep(1)
+                break
+
+    def maintain(self):
+        """
+        Reset the transition probabilities to their original values.
+        """
+        self.transition_probabilities = self.original_transition_probabilities
 
     def get_true_role(self):
         return self.role
@@ -75,13 +108,13 @@ class Sensor:
         return random.normalvariate(self.mean_value, self.standard_deviation)  # Normal distribution
 
     def send_data(self):
-        num_messages = random.randint(1, 10)
-        messages = [
-            {
-                'sensor_id': self.sensor_id,
-                'sensor_type': self.sensor_type.value,
-                'sensor_value': self.read_value(),
-                'timestamp': datetime.datetime.now().timestamp()
-            } for _ in range(num_messages)
-        ]
-        return messages
+        if (Globals.time % self.sampling_interval) != 0:
+            return
+        
+        message = {
+            'sensor_id': self.sensor_id,
+            'sensor_type': self.sensor_type.value,
+            'sensor_value': self.read_value(),
+            'timestamp': Globals.time
+        }
+        return message
