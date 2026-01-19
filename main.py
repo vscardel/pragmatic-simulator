b494dacb89ac5e0f2e527fc223a25983921a5c3b
João Victor <jv.joaocoelho@gmail.com>
2026-01-19 07:14:19 -0300

    add a venv and fastapi to project to make a frontend

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
+from typing import Union
+from fastapi import FastAPI
 from simulator import Simulator
                 
 
 if __name__ == "__main__":
     Simulator.get_instance().run()
         
+
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
