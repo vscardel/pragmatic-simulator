72950f8ac8cee258d3ce9bcd1a074fd223024330
João Victor <jv.joaocoelho@gmail.com>
2026-01-22 06:28:05 -0300

    add function add_relative_timer

--- utils/timers.py
+++ utils/timers.py
@@ -5,5 +5,8 @@ from typing import Callable
 def add_timer(time: int, func: Callable) -> None:
     globals.timers.append((time, func))
     
+def add_relative_timer(time: int, func: Callable) -> None:
+    globals.timers.append((globals.time + time, func))
+
 def remove_timer(time: int, func: Callable) -> None:
     globals.timers.remove((time, func))
