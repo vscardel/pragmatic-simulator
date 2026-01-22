46abca61117acf4b5b359988b03bfecb551c68a2
João Victor <jv.joaocoelho@gmail.com>
2026-01-22 06:27:00 -0300

    change broker dict to a class

--- broker.py
+++ broker.py
@@ -1,17 +1,27 @@
 import heapq
 import itertools
 from typing import Any
-from sensors import SensorRoleEnum, Sensor
+from sensors import SensorRoleEnum, Sensor, SensorMessage
 from typing import List, Tuple
 import random
 from collections import Counter
 from utils.colors import *
 import globals
+from production_plant import GlobalStateEnum
+
+
+class BrokerMessage:
+    def __init__(self, sensor_id: int, inferred_role: int, inferred_state: int, sensor_message: SensorMessage) -> None:
+        self.sensor_id = sensor_id
+        self.inferred_role = inferred_role
+        self.inferred_state = inferred_state
+        self.sensor_message = sensor_message
+
 
 class Broker:
     def __init__(self) -> None:
-        self.subscribers: List[int] = [] # lista de ID dos sensores
-        self.queue: List[Tuple[int, int, dict[str, Any]]] = []
+        self.subscribers: List[int] = []  # lista de ID dos sensores
+        self.queue: List[Tuple[int, int, BrokerMessage]] = []
         self.MAX_QUEUE_SIZE = 1000
         self.DROPPED_MESSAGES_COUNT = 0
         self.DROPPED_MESSAGES_BY_FULL_QUEUE = 0
@@ -23,11 +33,11 @@ class Broker:
     def subscribe(self, sensor: Sensor):
         self.subscribers.append(sensor.sensor_id)
 
-    def publish(self, sensor_id: int, data: dict[str, Any]) -> bool:
+    def publish(self, sensor_id: int, data: SensorMessage) -> bool:
         if (sensor_id not in self.subscribers):
             print(f"{RED}Sensor not subscribed to broker.{RESET}")
             return False
-        
+
         if len(self.queue) >= self.MAX_QUEUE_SIZE:
             self.DROPPED_MESSAGES_COUNT += 1
             self.DROPPED_MESSAGES_BY_FULL_QUEUE += 1
@@ -35,12 +45,21 @@ class Broker:
             self.ROUND_DROPPED_MESSAGES_BY_FULL_QUEUE += 1
             print(f"{YELLOW}Broker queue full. Dropping message.{RESET}")
             return False
-        
-        # TODO: infer sensor role by context instead of passing it randomly
+
+        # TODO: infer sensor role and state by context instead of passing it randomly
+        inferred_role = random.choice(list(SensorRoleEnum)).value
+        inferred_state = random.choice(list(GlobalStateEnum)).value
+
+        # TODO: the algorithm should define this number (this calcule is just a placeholder)
+        priority_number = inferred_role * inferred_state
+
         heapq.heappush(
             self.queue,
-            (random.choice(list(SensorRoleEnum)).value, next(self._counter), data)
-        )
+            (priority_number, next(self._counter), BrokerMessage(sensor_id=sensor_id,
+                                                                 inferred_role=inferred_role,
+                                                                 inferred_state=inferred_state,
+                                                                 sensor_message=data)))
+
         return True
 
     def print_queue_stats(self):
@@ -58,32 +77,29 @@ class Broker:
             print("Broker queue is empty.")
             return
 
-        enum_counts = Counter(item[0] for item in self.queue)
+        enum_counts = Counter(item.inferred_role for _,_,item in self.queue)
 
         print(f"{BLUE}=== Percentage by Sensor Role in Broker Queue ==={RESET}")
         for enum_val, count in enum_counts.items():
             percentage = (count / total_items) * 100
             print(f"{BLUE}Role {SensorRoleEnum(enum_val)}: {percentage:.4f}%{RESET}")
 
-        sensor_counts = Counter(item[2]['sensor_id'] for item in self.queue)
+        sensor_counts = Counter(item.sensor_id for _,_,item in self.queue)
 
         print(f"{BLUE}=== Percentage by Sensor in Broker Queue ==={RESET}")
         for sensor_id, count in sensor_counts.items():
             percentage = (count / total_items) * 100
             print(f"{BLUE}Sensor '{sensor_id}': {percentage:.4f}%{RESET}")
 
-    def flush(self) -> List[Tuple[int, int, dict[str, Any]]]:
+    def flush(self) -> List[BrokerMessage]:
         if (globals.time % 3600000 == 0):
             self.print_queue_stats()
-        
-        msgs = []
+
+        msgs: List[BrokerMessage] = []
         while self.queue:
-            msgs.append(heapq.heappop(self.queue))
-            
+            msgs.append(heapq.heappop(self.queue)[2])
+
         self.ROUND_DROPPED_MESSAGES_BY_FULL_QUEUE = 0
         self.ROUND_DROPPED_MESSAGES_COUNT = 0
-        
+
         return msgs
-        
-        
-        
